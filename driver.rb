93ac8b501de76bb20616aad6ba2d7bfaefc08a95
willgs <wgs12@pitt.edu>
2019-04-11 16:31:51 -0400

    Removed dummy files

--- driver.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'verifier'
-t = Verify.new
-
-result = "FIRST TEST: " + t.verify_second_pipeset("0", "0").to_s
-puts result
-
-result = "SECOND TEST: " + t.verify_fourth_pipeset('121.99', '121.12').to_s
-puts result
